2e912f002841182cde2988df67873a6c23beb37d
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-07-24 11:47:32 +0800

    first commit

---
+++ seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name) VALUES ("Human Resources");
+INSERT INTO department (name) VALUES ("Marketing");
+INSERT INTO department (name) VALUES ("Information Technology");
+INSERT INTO department (name) VALUES ("finance");
+
+INSERT INTO role (title, salary, department_id) VALUES ("HR-manger", 80, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Tester", 70, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Developer", 90, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Director", 120, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Manager", 100, 4);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Krishnan", "Jishnu", 2);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Sumi", "Shradha", 1);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Dhanya", "Ahalya", 3);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Jyothi", "Shainy", 4);
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("Adithi", "Ananth", 5);
